14fbd3d66a5389571aa84c11aa2cc63fc2bc1432
supersoaker <visionsoap@yahoo.de>
2014-01-06 10:41:13 +0100

    DB

---
+++ script/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: mruescher
+ * Date: 06.01.14
+ * Time: 10:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+// document ready
+$(function(){
+
+
+});
